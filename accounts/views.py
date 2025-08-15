bbe3428106d174cd268f39ae3d719124daca6f65
Ilya <j.stur2502@gmail.com>
2025-08-15 23:30:19 +0300

    mini-fix view

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,6 @@ def signup(request):
             user = form.save()
             login(request, user)
             return redirect('home')
-        else:
+    else:
             form = SignUpForm()
-        return render(request, 'accounts/signup.html', {'form': form})
+    return render(request, 'accounts/signup.html', {'form': form})
